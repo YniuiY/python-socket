32c18131dc04895cdd2ccf3c444da4146f9fa3c6
YniuiY <49220959+YniuiY@users.noreply.github.com>
2022-03-18 10:31:44 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 host = '127.0.0.1'
 port = 50007
-name = "yang di"
+name = "zhou jie lun"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 data = input('>')
